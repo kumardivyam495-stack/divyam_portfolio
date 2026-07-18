6c14918ae05fb60ed3f92168179761560d1ebd8f
Divyam <kumardivyam495@gmail.com>
2026-07-18 13:46:47 +0530

    chore: dynamically set basePath for local dev and production build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
-const basePath = "/divyam_portfolio";
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/divyam_portfolio" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
   basePath: basePath,
+  trailingSlash: true,
   env: {
     NEXT_PUBLIC_BASE_PATH: basePath,
   },
